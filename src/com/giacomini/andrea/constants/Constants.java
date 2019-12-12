e52c9fdec8af57b56e6129eb3ce395082939a5f9
andgiacomini <andgiacomini@L-IT-14732.europe.corp.altran.com>
2019-12-12 10:49:22 +0100

    .test

--- src/com/giacomini/andrea/constants/Constants.java
+++ src/com/giacomini/andrea/constants/Constants.java
@@ -17,4 +17,5 @@ public class Constants
 	public static final String ORACLEQUERY_FINDBYID = "SELECT * FROM <table_name> WHERE <column_name> = :id";
 	
 	//---------------------------- ./Oracle Query -----------------------------------//
+	
 }
