0b669ba10aa03f6c09368c33352f93103cb67d6b
andgiacomini <andgiacomini@L-IT-14732.europe.corp.altran.com>
2019-12-12 10:46:50 +0100

    .

--- src/com/giacomini/andrea/constants/Constants.java
+++ src/com/giacomini/andrea/constants/Constants.java
@@ -1,5 +1,20 @@
 package com.giacomini.andrea.constants;
 
-public class Constant {
-
+public class Constants 
+{
+	//------------------------ Oracle JDBC Connection -------------------------------//
+	
+	public static final String ORACLEURL = "jdbc:oracle:thin:@192.168.2.163:1521:<ORACLE_DB_NAME>";
+	public static final String ORACLEUSER = "<ORACLE_USER>";
+	public static final String ORACLEPWD = "<ORACLE_PWD>";
+	public static final String ORACLEDRIVER = "oracle.jdbc.driver.OracleDriver";
+	
+	//---------------------- ./Oracle JDBC Connection -------------------------------//
+	
+	//----------------------------- Oracle Query ------------------------------------//
+	
+	public static final String ORACLEQUERY_FINDALL = "SELECT * FROM <table_name>";
+	public static final String ORACLEQUERY_FINDBYID = "SELECT * FROM <table_name> WHERE <column_name> = :id";
+	
+	//---------------------------- ./Oracle Query -----------------------------------//
 }
